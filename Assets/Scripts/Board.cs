895d2fdf1cfda6d2d3cbea8bafbd9eb4bea70b1a
Fredric Dorothy <fredric.dorothy@vipaar.com>
2019-04-26 22:30:09 -0500

    added basic board loading from tiled2unity

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+public enum PieceType {
+    MOUSE = 0,
+    GOBLIN,
+    WALL,
+    EMPTY
+}
+
+public class Piece {
+    public PieceType t;
+    public int id;
+}
+
+public class Board
+{
+    public int w = 24;
+    public int h = 24;
+    public Piece[] board;
+
+    public Board(int w, int h)
+    {
+        this.w = w;
+        this.h = h;
+        board = new Piece[w*h];
+    }
+
+    public void setPiece(int x, int y, Piece val) {
+        board[x + y * w] = val;
+    }
+
+    public Piece getPiece(int x, int y) {
+        return board[x + y * w];
+    }
+
+    public void Log() {
+        UnityEngine.Debug.Log("board width / height: " + w + ", " + h);
+        for (int x = 0; x < w; x++) {
+            string line = "";
+            for (int y = 0; y < h; y++) {
+                Piece p = getPiece(x, y);
+                string output = "  ";
+                if (p != null)
+                {
+                    switch (p.t)
+                    {
+                        case PieceType.WALL:
+                            output = "w  ";
+                            UnityEngine.Debug.Log("wall");
+                            break;
+                        case PieceType.GOBLIN:
+                            output = "g" + p.id;
+                            break;
+                        case PieceType.MOUSE:
+                            output = "m" + p.id;
+                            break;
+                        default: break;
+                    }
+                }
+                line = line + output + ",";
+            }
+            UnityEngine.Debug.Log(line);
+        }
+    }
+}
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Game : MonoBehaviour
+{
+    public Tilemap obstacles;
+    public Tilemap mice;
+    public Tilemap goblins;
+    public Board board;
+
+    void Start()
+    {
+        LoadFromTiled();
+    }
+
+    void LoadFromTiled() {
+        board = new Board(obstacles.size.x, obstacles.size.y);
+        int id = 0;
+        for (int i = 0; i < board.w; i++) {
+            for (int j = 0; j < board.h; j++) {
+                Vector3Int cell = new Vector3Int(obstacles.cellBounds.xMin + i, obstacles.cellBounds.yMin+j, (int)obstacles.cellBounds.zMin);
+                if (obstacles.HasTile(cell)) {
+                    Piece piece = new Piece();
+                    piece.t = PieceType.WALL;
+                    board.setPiece(i, j, piece);
+                } else if (mice.HasTile(cell)) {
+                    Piece piece = new Piece();
+                    piece.t = PieceType.MOUSE;
+                    piece.id = id;
+                    board.setPiece(i, j, piece);
+                    id++;
+                } else if (goblins.HasTile(cell))
+                {
+                    Piece piece = new Piece();
+                    piece.t = PieceType.GOBLIN;
+                    piece.id = id;
+                    board.setPiece(i, j, piece);
+                    id++;
+                }
+            }
+        }
+        mice.gameObject.SetActive(false);
+        goblins.gameObject.SetActive(false);
+    }
+}
